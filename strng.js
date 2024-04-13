3fe7829ac310efa3d8fadc816a1586a14825d8fc
pat104471 <165080644+pat104471@users.noreply.github.com>
2024-04-13 09:32:20 -0500

    Add files via upload

---
+++ strng.js
@@ -0,0 +1,32 @@
+
+
+
+function isPalindrome(str) {
+    
+
+
+
+    let rev = str.split("").reverse().join("");
+ 
+    if (rev == str1) {
+        return true
+    }
+    return false
+    
+ 
+}
+
+
+// take input
+let str1 = prompt('Enter a string: ');
+
+
+ 
+console.log(isPalindrome(str1));
+
+
+
+
+// call the function
+const value = isPalindrome(str1);
+
