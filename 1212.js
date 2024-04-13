cabd3432d0f87aa85da4afb55e10ae59ee69e6fe
pat104471 <165080644+pat104471@users.noreply.github.com>
2024-04-13 18:13:00 -0500

    Add files via upload

---
+++ 1212.js
@@ -0,0 +1,48 @@
+// function to combine
+function combineNames(){
+    const firstName=
+    document.getElementById('firstName').Value.trim();
+    const lastName=
+    document.getElementById('lastName').Value.trim();
+    return firstName + '' (lastName);{
+}
+}
+//function validateZipCode(zipCode){
+
+//}
+
+//Show message
+function showSecretMessage(){
+    const fullName = document.getElementById('firstName + lastName').value.trim();
+    const zipCode = document.getElementById('zipCode').value.trim();
+   
+
+}
+
+document.getElementById("info1").addEventListener ("change"), function() {
+    let v = parseInt(this.value);
+    if (v < 0) this.value = 0;
+    if (v > 5) this.value = 5;
+
+     
+    function myFunction() {
+        alert("Congrats");
+    }
+}
+  
+
+
+//check zip code
+//if (!validateZipCode(zipCode)) {
+    //alert('invalid');
+    //return;
+//}
+
+
+//validateZipCode();
+combineNames();
+showSecretMessage();
+
+function myFunction() {
+    alert("Congrats");
+}
