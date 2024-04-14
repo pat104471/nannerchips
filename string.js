dac5f1b6721935589a0c288f8c6e58048c3799ca
pat104471 <165080644+pat104471@users.noreply.github.com>
2024-04-13 22:44:31 -0500

    Add files via upload

---
+++ string.js
@@ -0,0 +1,50 @@
+
+
+
+
+
+
+
+
+function isPalindrome() {
+
+//remove spaces
+ const cleanString = string.replace();
+
+    //let rev = string.split("").reverse().join("");
+    return cleanString === cleanString.split('').reverse().join('');
+    }
+
+//main prompt user input
+function checkPalindrome(){
+    const inputString = document.getElementById('Palindrome').value; 
+}
+            
+                alert('the string is a palindrom');{
+
+                }
+                    
+            if (isPalindrome (Palindrom)) {
+                alert("The string is a palindrome");
+            }
+
+        
+            else  {
+                    alert('the string is not a palindrom'); //notify if not
+            }  
+                    if (!again) {
+                        continueLoop = false;
+                    
+                    }
+                    else {
+                        continueLoop = false;
+
+                    }
+             console.log(isPalindrome)   
+                
+        
+isPalindrome();
+// call the function
+checkPalindrome();
+
+
