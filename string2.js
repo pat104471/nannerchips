5bab7716c4f62d1f4fb74b9cd03cdfb505226aca
pat104471 <165080644+pat104471@users.noreply.github.com>
2024-04-13 22:43:47 -0500

    Add files via upload

---
+++ string2.js
@@ -0,0 +1,69 @@
+// function to combine
+function combineNames(){
+    const firstName=
+    document.getElementById('firstName').Value.trim();
+    const lastName=
+    document.getElementById('lastName').Value.trim();
+    return firstName + '' (lastName);{
+}
+}
+console.log()
+
+//function validateZipCode(zipCode){
+
+
+
+//Show message
+function showSecretMessage(){
+    const fullName = document.getElementById('firstName + lastName').value.trim();
+    const zipCode = document.getElementById('zipCode').value.trim();
+   console.log()
+
+}
+
+document.getElementById("zipCode").addEventListener ("change"), function() {
+
+    let v = parseInt(this.value);
+    if (v < 0) this.value = 0;
+    if (v > 5) this.value = 5;
+
+
+    if (maxlegth) this.value = 0;
+    if (minlegth) this.value = 5;
+}
+
+
+
+ alert('the string is a palindrom');{
+
+                }
+                    
+            if (isPalindrome (Palindrom)) {
+                alert("The string is a palindrome");
+            }
+
+        
+            else  {
+                    alert('the string is not a palindrom'); //notify if not
+            }  
+                    if (!again) {
+                        continueLoop = false;
+                    
+                    }
+                    else {
+                        continueLoop = false;
+
+                    }
+             console.log(isPalindrome)  
+
+
+  
+
+
+
+combineNames();
+showSecretMessage();
+
+function myFunction() {
+    alert("Congrats");
+}
