2986fe1622b49ff0901256c4cad55efa7c2f1633
pat104471 <165080644+pat104471@users.noreply.github.com>
2024-04-15 16:29:57 -0500

    Add files via upload

--- string.js
+++ string.js
@@ -6,45 +6,29 @@
 /*Pat Schierman*/
 
 
-function isPalindrome() {
+function checkPalindrome() {}
 
 //remove spaces
- const cleanString = string.replace();
+ const inputString = document.getElementById('Palindrome');
 
-    //let rev = string.split("").reverse().join("");
+if (isPalindrome(inputString)){
+    alert("the string is a palindrome");
+}
+else{
+    alert("the string is not a palindrome");
+} 
+function isPalindrome(string) {
+    const cleanString = string.replace('');
+
     return cleanString === cleanString.split('').reverse().join('');
-    }
+}
+
 
 //main prompt user input
-function checkPalindrome(){
-    const inputString = document.getElementById('Palindrome').value; 
-}
-            
-                alert('the string is a palindrom');{
-
-                }
-                    
-            if (isPalindrome (Palindrom)) {
-                alert("The string is a palindrome");
-            }
-
-        
-            else  {
-                    alert('the string is not a palindrom'); //notify if not
-            }  
-                    if (!again) {
-                        continueLoop = false;
-                    
-                    }
-                    else {
-                        continueLoop = false;
-
-                    }
-             console.log(isPalindrome)   
-                
-        
-isPalindrome();
-// call the function
 checkPalindrome();
+
+
+
+
 
 
